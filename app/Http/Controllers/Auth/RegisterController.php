7a87f66ce9359a40cb1b3d815d170f41659afd0e
ahmeddhamdallah <ahmed.d.hamdallah@gmail.com>
2018-11-23 16:36:46 +0200

    Edit laravel login to be with national_id and password instead of email and password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'national_id' => 'required|integer|max:20|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -65,7 +65,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'national_id' => $data['national_id'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->integer('national_id')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

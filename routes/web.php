5692671fea3c5866c3412ed8e1ef5d9f4767e449
ahmeddhamdallah <35041899+ahmeddhamdallah@users.noreply.github.com>
2018-11-28 13:30:25 +0200

    Set the routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    $threads=App\Thread::paginate(15);
+    return view('welcome',compact('threads'));
 });
+
 
 Auth::routes();
 
@@ -21,3 +26,24 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::get('thread/search','ThreadController@search');
+
+Route::post('/thread/mark-as-solution','ThreadController@markAsSolution')->name('markAsSolution');
+Route::resource('/thread','ThreadController');
+
+
+Route::resource('comment','CommentController',['only'=>['update','destroy']]);
+
+Route::post('comment/create/{thread}','CommentController@addThreadComment')->name('threadcomment.store');
+
+Route::post('reply/create/{comment}','CommentController@addReplyComment')->name('replycomment.store');
+
+
+Route::post('comment/like','LikeController@toggleLike')->name('toggleLike');
+
+Route::get('/user/profile/{user}', 'UserProfileController@index')->name('user_profile')->middleware('auth');
+
+Route::get('/markAsRead',function(){
+    auth()->user()->unreadNotifications->markAsRead();
+});
